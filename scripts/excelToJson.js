4e57712125e5d77da5a9bd3c07d1f29ac04fb898
Peter Laske <37439758+laske185@users.noreply.github.com>
2024-04-25 09:52:18 +0200

    improved build scripts

--- scripts/excelToJson.js
+++ scripts/excelToJson.js
@@ -2,7 +2,7 @@ const readXlsxFile = require('read-excel-file/node');
 const fs = require('fs');
 const path = require('path');
 
-readXlsxFile(path.resolve(process.cwd(), 'public/config/katalog-v0.996.xlsx'), {
+readXlsxFile(path.resolve(process.cwd(), 'public/config/Standardanforderungskatalog_V1.0.xlsx'), {
 	sheet: 'DATEN',
 }).then((data) => {
 	fs.writeFileSync(path.resolve(process.cwd(), 'src/services/config/katalog.json'), JSON.stringify(data, null, 2));
