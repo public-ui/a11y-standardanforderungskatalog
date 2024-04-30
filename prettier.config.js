ba66017cfa72a1ea217cd356805d04a0994e9483
Martin <6279703+deleonio@users.noreply.github.com>
2024-04-30 08:45:53 +0200

    chore: update deps

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
 	printWidth: 160,
 	singleQuote: true,
 	useTabs: true,
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,8 @@
-module.exports = (...args) => {
-	const config = require('@leanup/stack-react/webpack.config')(...args);
-	const UnoCSS = require('@unocss/webpack').default;
+import Config from '@leanup/stack-react/webpack.config.js';
+import UnoCSS from '@unocss/webpack';
 
-	if (args[0].WEBPACK_BUILD) {
-		const path = require('path');
-		const WebpackPwaManifest = require('webpack-pwa-manifest');
-		const pwaManifestConfigPath = path.resolve(process.cwd(), 'pwa-manifest.config.js');
-		const { GenerateSW } = require('workbox-webpack-plugin');
-		const workboxConfigPath = path.resolve(process.cwd(), 'workbox-config.js');
-		config.plugins.push(new WebpackPwaManifest(require(pwaManifestConfigPath)));
-		config.plugins.push(new GenerateSW(require(workboxConfigPath)));
-	}
-
+export default (...args) => {
+	const config = Config(...args);
 	config.plugins.push(UnoCSS());
-
 	return config;
 };
